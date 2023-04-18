ef313e38145b704471f1c02d80a82e58b762b3f8
Diana <102248347+dididiana07@users.noreply.github.com>
2023-04-18 13:56:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,100 @@
 import os
-import requests
 import datetime
-from twilio.rest import Client
+import smtplib
 
-current_date = datetime.datetime.now()
-sheety_endpoint = "https://api.sheety.co/1022e180f0ac414bc9f54f2e2113f351/flightDeals/flights"
-ACCOUNT_SID, AUTH_TOKEN = os.environ["ACCOUNT_SID"], os.environ["AUTH_TOKEN"]
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+import requests
 
-apikey = os.environ["KIWI_API_KEY"]
-endpoint = "https://api.tequila.kiwi.com/v2/search"
-headers = {
-    "apikey": f"{apikey}"
-}
+YAHOO_EMAIL = os.environ["YAHOO_EMAIL"]
+AUTH_YAHOO = os.environ["AUTH_YAHOO"]
+TEQUILA_API_KEY = os.environ["KIWI_API_KEY"]
 
-
-def extract_data_from_sheet(url_sheety):
-    """Extract the flights key from your Google sheet.
-    Return a list of dictionaries in json format."""
-    flights_sheet = requests.request(method="GET",
-                                     url=url_sheety)
-    flights = flights_sheet.json()["flights"]
-    return flights
+SHEETY_ENDPOINT_FLIGHTS = ""
+SHEETY_ENDPOINT_USERS = ""
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
 
 
-my_flights = extract_data_from_sheet(url_sheety=sheety_endpoint)
+def check_email(email):
+    """Unless the email matches, keep asking for it."""
+    while True:
+        repeat_email = input("Reenter the email:\n")
+        if repeat_email == email:
+            return True
 
 
-for flight in my_flights:
-    city = flight["city"]
-    iata_code = flight["iataCode"]
-    price = flight["lowestPrice"]
+def create_user():
+    """Get the data for a new user."""
+    print("Welcome to Diana's Flight Club.\n"
+          "We find the best flight deals and email you.")
+    name = input("What is your first name?\n")
+    last_name = input("What is your last name?\n")
+    email = input("What is your email?\n")
+    if check_email(email):
+        print("Thanks for subscribing to our deals.")
+        return name, last_name, email
 
-    parameters = {
-        "fly_from": "BCN",
-        "fly_to": f"{iata_code}",
-        "date_from": f"{current_date.day}/{current_date.month}/{current_date.year}",
-        "date_to": f"{current_date.day}/{(current_date.month + 6) % 12}/{current_date.year}",
-        "price_from": "0",
-        "price_to": f"{price}"
+
+def add_user(**kwargs):
+    """Add the user to the Google sheet."""
+    new_user = {
+        "user":
+            {"name": kwargs["name"],
+             "lastName": kwargs["last_name"],
+             "email": kwargs["email"]}
     }
-    kiwi_response = requests.get(url=endpoint, headers=headers, params=parameters)
-    kiwi_data = kiwi_response.json()
-    try:
-        search = kiwi_data["data"][0]
-    except IndexError:
-        pass
-    else:
-        messages = client.messages.create(from_=os.environ["TWILIO_PHONE_NUMBER"],
-                                          to="",
-                                          body="Hey✈️\n"
-                                               f"I just found a flight to {city} for the day"
-                                               f" {search['local_departure'].split('T')[0]} and for "
-                                               f"the price of {search['price']}€.\n"
-                                               f"Click link to see more: {search['deep_link']}")
+    response = requests.request(method="POST", url=SHEETY_ENDPOINT_USERS, json=new_user)
+    print(response.status_code)
 
+
+def get_users_data() -> list:
+    """Retrieve all the emails for each user subscribed."""
+    response = requests.request(method="GET", url=SHEETY_ENDPOINT_USERS)
+    data = [user["email"] for user in response.json()["users"]]
+    return data
+
+
+def get_flights_info() -> list:
+    """Returns a list containing dictionaries."""
+    response = requests.request(method="GET", url=SHEETY_ENDPOINT_FLIGHTS)
+    data = response.json()["flights"]
+    return data
+
+
+def send_mails(users: list, flights_data: list):
+    current_date = datetime.datetime.now()
+    headers = {
+        "apikey": f"{TEQUILA_API_KEY}"
+    }
+    for flight in flights_data:
+        city = flight["city"]
+        iata_code = flight["iataCode"]
+        price = flight["lowestPrice"]
+        parameters = {
+            "fly_from": "BCN",
+            "fly_to": f"{iata_code}",
+            "date_from": f"{current_date.day}/{current_date.month}/{current_date.year}",
+            "date_to": f"{current_date.day}/{(current_date.month + 6) % 12}/{current_date.year}",
+            "price_from": "0",
+            "price_to": f"{price}"
+        }
+        response = requests.get(url=TEQUILA_ENDPOINT,
+                                headers=headers,
+                                params=parameters)
+        data = response.json()
+        try:
+            search = data["data"][0]
+        except IndexError:
+            pass
+        else:
+            for user in users:
+                with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+                    connection.starttls()
+                    connection.login(user=YAHOO_EMAIL, password=AUTH_YAHOO)
+                    connection.sendmail(from_addr=YAHOO_EMAIL,
+                                        to_addrs=user,
+                                        msg=f"Subject:New Deals Alert\n\nI just found a flight to {city} for the day"
+                                            f" {search['local_departure'].split('T')[0]} and for the "
+                                            f"price of {search['price']} EUR. "
+                                            f"\nClick link to see more: {search['deep_link']}")
+
+
+send_mails(users=get_users_data(), flights_data=get_flights_info())
